087581302de610ac2b88d9dcb7d6bce66c491834
dipendrasingh100 <dipsingh9950@gmail.com>
2023-08-16 14:27:52 +0530

    gif added in advertise

--- src/components/Advertise.js
+++ src/components/Advertise.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "../css/home.css"
-// import ad from '../assets/giphy-ad.gif'
+import ad from '../assets/giphy-ad.gif'
 
 const Advertise = () => {
   return (
     <div className="rs-ad-container">
-      <div className="ad-title" style={{ height: '100%' }}>
-        {/* <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img> */}
-        Advertisement
+      <div style={{ height: '100%' }}>
+        <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img>
       </div>
     </div>
   );
