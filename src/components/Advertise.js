6c544a5e5ea10c955434cae99de9527ee4d8b1b8
dipendrasingh100 <dipsingh9950@gmail.com>
2023-08-16 14:32:25 +0530

    added border and text to ad

--- src/components/Advertise.js
+++ src/components/Advertise.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "../css/home.css"
-import ad from '../assets/giphy-ad.gif'
+// import ad from '../assets/giphy-ad.gif'
 
 const Advertise = () => {
   return (
     <div className="rs-ad-container">
       <div style={{ height: '100%' }}>
-        <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img>
+        {/* <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img> */}
+        Advertisement
       </div>
     </div>
   );
