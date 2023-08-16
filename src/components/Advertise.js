2d6d4d6b94582e430cde2e6efada39ac64ebd202
dipendrasingh100 <dipsingh9950@gmail.com>
2023-08-16 13:14:38 +0530

    updated advertise with border

--- src/components/Advertise.js
+++ src/components/Advertise.js
@@ -6,7 +6,8 @@ const Advertise = () => {
   return (
     <div className="rs-ad-container">
       <div className="ad-title" style={{ height: '100%' }}>
-        <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img>
+        {/* <img src={ad} style={{ width: "27vw", height: "100%", border:'none', maxWidth:"328px" }} alt="ad"></img> */}
+        Advertisement
       </div>
     </div>
   );
